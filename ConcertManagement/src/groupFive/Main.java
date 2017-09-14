dbbee3ade00795eb2d27819b3dbc62d5a24234d5
Tinus Flagstad <tinus@flagstad.as>
2017-09-14 13:58:18 +0200

    La til artister i en drop down menu.

--- ConcertManagement/src/groupFive/Main.java
+++ ConcertManagement/src/groupFive/Main.java
@@ -35,4 +35,14 @@ public class Main extends Application {
             e.printStackTrace();
         }
     }
+
+    public static ArrayList<String> filterList(ArrayList<String> wholeList, String split) {
+        ArrayList<String> listToAdd = new ArrayList<>();
+        for (int x  = 0; x < wholeList.size(); x++) {
+            if (wholeList.get(x).startsWith(split)) {
+                listToAdd.add(wholeList.get(x).replace(split, ""));
+            }
+        }
+        return listToAdd;
+    }
 }
