00c7346da5f98bf5f19c21c1e8faa0af023e1a74
Tinus Flagstad <tinus@flagstad.as>
2017-10-02 13:40:06 +0200

    Ryddet opp litt i koden til arrangor_controller.

--- ConcertManagement/src/groupFive/Arrangor_controller.java
+++ ConcertManagement/src/groupFive/Arrangor_controller.java
@@ -27,37 +27,6 @@ import static util.Filter.getAllFestivalsObservableList;
 
 public class Arrangor_controller {
     @FXML
-    private ListView listViewScene1;
-    @FXML
-    private ListView listViewScene2;
-    @FXML
-    private ListView listViewScene3;
-
-    // De 3 listviewsene i en liste.
-    private List<ListView> listViews;
-
-    @FXML
-    private ListView listViewDatesScene1;
-    @FXML
-    private ListView listViewDatesScene2;
-    @FXML
-    private ListView listViewDatesScene3;
-
-    //De 3 datoviewsene settes i en liste
-    private List<ListView> dateListViews;
-
-    @FXML
-    private TextField textFieldScene1;
-
-    @FXML
-    private TextField textFieldScene2;
-
-    @FXML
-    private TextField textFieldScene3;
-
-    //3 textfields i en liste
-    //private List<TextField> textFields;
-    @FXML
     private ScrollPane scrollPane;
 
     @FXML
@@ -79,13 +48,11 @@ public class Arrangor_controller {
 
     @FXML
     public void initialize() {
-        listViews = Arrays.asList(listViewScene1, listViewScene2, listViewScene3);
-        dateListViews = Arrays.asList(listViewDatesScene1, listViewDatesScene2, listViewDatesScene3);
-        //textFields = Arrays.asList(textFieldScene1, textFieldScene2, textFieldScene3);
         addItemsToList();
     }
 
     private void createChoiceBoxListener() {
+        // Endrer festivalSelected når du velger en ny festival i choicebox.
         choiceBoxFestivals.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
@@ -106,22 +73,20 @@ public class Arrangor_controller {
     }
 
     private void changedFestOrScene() {
+        // Disse strengene er hvilken du har valgt i choiceboxen.
         String festival = choiceBoxFestivals.getItems().get(festivalSelected).toString();
         String scene = choiceBoxScenes.getItems().get(sceneSelected).toString();
 
-        putConcertsInSceneLists(festival);
-        putConcertDatesInSceneLists(festival);
         putSceneNamesInTextBox(festival, scene);
     }
 
     private void addItemsToList() {
+        // Denne putter bare default verdier inn i alle knapper lister etc.
         putFestivalsInChoiceBox();
         putScenesInChoiceBox(Main.festivals.get(0).getFestival());
         createChoiceBoxListener();
         createSceneBoxListener();
-        putConcertsInSceneLists(Main.festivals.get(0).getFestival()); //Dette er default festivalen som blir først markert, kan kanskje endre denne til bare første i listen.
-        //putConcertDatesInSceneLists("UKA 2017");
-        //TODO: add alle andre ting vi må putte i lister her som de forksjellige scenene etc.
+        putSceneNamesInTextBox(Main.festivals.get(0).getFestival(), Main.festivals.get(0).getScene().get(0).getNavn());
     }
 
     private void putScenesInChoiceBox(String festival) {
@@ -129,10 +94,7 @@ public class Arrangor_controller {
 
         for (Festival f : Main.festivals) {
             if (f.getFestival().equals(festival)) {
-                System.out.println(f.getScene().size());
                 for (int i = 0; i < f.getScene().size(); i++) {
-                    System.out.println(i);
-                    System.out.println(f.getScene().get(i).getNavn());
                     sceneNames.add(f.getScene().get(i).getNavn());
                 }
             }
@@ -147,7 +109,6 @@ public class Arrangor_controller {
         choiceBoxFestivals.setItems(observableListToAdd);
         choiceBoxFestivals.getSelectionModel().selectFirst();
         // Denne linjen gjør bare at det første itemet i listen blir vist og selected.
-        // TODO: gjør listen klikkbar og endre ting i scene boksen videre.
     }
 
     private void putSceneNamesInTextBox(String festival, String scene) {
@@ -156,80 +117,22 @@ public class Arrangor_controller {
 
         for (Festival f : Main.festivals) {
             if (f.getFestival().equals(festival)) {
-                System.out.println(f.getScene());
                 for (int i = 0; i < f.getScene().size(); i++) {
                     if(scene.equals(f.getScene().get(i).getNavn())) {
                         List<Concert> concerts = f.getScene().get(i).getKonsert();
                         for (int n = 0; n < concerts.size(); n++) {
-                            System.out.println(concerts.get(n).getArtist());
                             Button btn = createButton(concerts.get(n).getArtist());
                             btn.setId("arrScenes");
                             vbox.getChildren().add(btn);
                         }
                     }
                 }
-                /*for (int i = 0; i < 3; i++) {
-                    //textFields.get(i).setText(f.getScene().get(i).getNavn());
-                }*/
             }
         }
         scrollPane.setContent(vbox);
     }
 
-    private void putConcertsInSceneLists(final String festival) {
-        putSceneNamesInTextBox(festival, "Dødens dal");
-        for (Festival f : Main.festivals) {
-            if (f.getFestival().equals(festival)) {
-                // Når du har funnet riktig festival.
-                for (int i = 0; i < 3; i++) {
-                    final int iFinal = i;
-                    // Denne må være final for å kunne bruke i en anonym klasse.
-
-                    /*listViews.get(i).setEditable(true);
-                    List<String> concerts = new ArrayList<>();
-                    for (Concert c : f.getScene().get(i).getKonsert()) {
-                        concerts.add(c.getArtist());
-                    }
-                    ObservableList<String> observableListToAdd = FXCollections.observableArrayList(concerts);
-                    listViews.get(i).setItems(observableListToAdd);
-                    listViews.get(i).setOnMouseClicked(new EventHandler<MouseEvent>() {
-                        @Override
-                        public void handle(MouseEvent event) {
-                            try {
-                                String itemClicked = listViews.get(iFinal).getSelectionModel().getSelectedItem().toString();
-                                int whichScene = iFinal;
-                                showArbeidere(itemClicked, whichScene, festival);
-                            } catch (Exception e) {
-                                System.out.println("Du har ikke valgt en konsert.");
-                            }
-
-
-                        }
-                    });*/
-                }
-            }
-        }
-    }
-
-    private void putConcertDatesInSceneLists(String festival) {
-        putSceneNamesInTextBox(festival, "Dødens dal");
-        for (Festival f : Main.festivals) {
-            if (f.getFestival().equals(festival)) {
-                // Når du har funnet riktig festival.
-                for (int i = 0; i < 3; i++) {
-                    List<String> concertDates = new ArrayList<>();
-                    for (Concert c : f.getScene().get(i).getKonsert()) {
-                        concertDates.add(c.getDato());
-                    }
-                    ObservableList<String> observableListToAdd = FXCollections.observableArrayList(concertDates);
-                    //dateListViews.get(i).setItems(observableListToAdd);
-                }
-            }
-        }
-    }
-
     private void showArbeidere(String concert, int whichScene, String festival) {
-        // TODO: lag popupen finere.
         for (Festival f : Main.festivals) {
             if (f.getFestival().equals(festival)) {
                 for (Concert c : f.getScene().get(whichScene).getKonsert()) {
@@ -238,52 +141,16 @@ public class Arrangor_controller {
                         String lystekniker = "Lysteknikere\n";
                         String lydtekniker = "Lydteknikere\n";
 
-
-                        /*Stage stage = new Stage(StageStyle.DECORATED);
-                        stage.setX(400);
-                        stage.setY(400);
-                        // Hvilke kordinater det nye vinduet skal åpnes i.
-
-                        Pane layout = new AnchorPane();
-                        layout.setPrefSize(500, 250);
-                        stage.setScene(new Scene(layout));
-                        // lager ny layout og scene som er 500px bred og 270px høy.
-
-                        ListView listViewSoundTechs = new ListView();
-                        listViewSoundTechs.setPrefSize(250, 250);
-                        listViewSoundTechs.setEditable(true);*/
-
-                        //List<String> soundTechs = new ArrayList<>();
-                        //soundTechs.add("SoundTechs: ");
                         for (SoundTech st : c.getLyd()) {
                             lystekniker += st.getNavn() + "\n";
-                            //soundTechs.add(st.getNavn());
                         }
-                        //ObservableList<String> observableListSoundTechs = FXCollections.observableArrayList(soundTechs);
-                        //listViewSoundTechs.setItems(observableListSoundTechs);
-                        // Lager ny listview og går gjennom alle SoundTechsene og legger de til i listen.
 
-
-                        /*ListView listViewLightTechs = new ListView();
-                        listViewLightTechs.setPrefSize(250, 250);
-                        listViewLightTechs.setLayoutX(250);
-                        listViewLightTechs.setEditable(true);
-
-                        List<String> LightTechs = new ArrayList<>();*/
-                        //LightTechs.add("LightTechs: ");
                         for (LightTech lt : c.getLys()) {
                             lydtekniker += lt.getNavn() + "\n";
                         }
+
                         labelLightTech.setText(lystekniker);
                         labelSoundTech.setText(lydtekniker);
-                        /*ObservableList<String> observableListLightTechs = FXCollections.observableArrayList(LightTechs);
-                        listViewLightTechs.setItems(observableListLightTechs);
-                        // Lager ny listview og går gjennom alle LightTechsene og legger de til i listen.
-
-                        layout.getChildren().add(listViewSoundTechs);
-                        layout.getChildren().add(listViewLightTechs);
-                        // Legger til listene i layouten og viser den.
-                        stage.show();*/
                     }
                 }
             }
@@ -297,7 +164,6 @@ public class Arrangor_controller {
         button.setOnMouseClicked(event -> {
             try {
                 String festival = choiceBoxFestivals.getItems().get(festivalSelected).toString();
-                String scene = choiceBoxScenes.getItems().get(sceneSelected).toString();
                 showArbeidere(name, sceneSelected, festival );
             } catch (Exception e) {
                 e.printStackTrace();
@@ -317,7 +183,6 @@ public class Arrangor_controller {
     @FXML
     private void goHome(){
         Main main = new Main();
-        //Constants.emptyStack(); Jeg kommenterte ut linjen som ikke virker.
         main.changeView(rootPane, Constants.getHome());
     }
 
