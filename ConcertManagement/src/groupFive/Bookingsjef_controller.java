621f9301ed742e136b26e725ce59901958b01c43
Pål Larsen <paaledward@gmail.com>
2017-10-12 15:10:53 +0200

    Gjort det slik at 'Logg ut' knappen fungerer

--- ConcertManagement/src/groupFive/Bookingsjef_controller.java
+++ ConcertManagement/src/groupFive/Bookingsjef_controller.java
@@ -1,5 +1,23 @@
 package groupFive;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import util.Constants;
+
 public class Bookingsjef_controller {
+
+    @FXML
+    private AnchorPane rootPane;
+
+    public void initialize() {
+
+    }
+
+    @FXML
+    private void goBack(){
+        String fxmlFileName = "Main";
+        Main main = new Main();
+        main.changeView(rootPane, fxmlFileName);
+    }
 
 }
