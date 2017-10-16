34136c99820956a405e2e31a5f323c33d347dc5a
Tinus Flagstad <tinus@flagstad.as>
2017-10-16 10:29:16 +0200

    startet såvidt å gjøre ID12

--- ConcertManagement/src/groupFive/Bookingsjef_controller.java
+++ ConcertManagement/src/groupFive/Bookingsjef_controller.java
@@ -1,16 +1,29 @@
 package groupFive;
 
+import Json.Concert;
+import Json.Festival;
+import Json.Scene;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 import util.Constants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static util.Filter.getAllBandsObservableList;
 
 public class Bookingsjef_controller {
 
     @FXML
     private AnchorPane rootPane;
 
-    public void initialize() {
+    @FXML
+    private VBox vBoxBands;
 
+    public void initialize() {
+        putItemsInLists();
     }
 
     @FXML
@@ -20,4 +33,22 @@ public class Bookingsjef_controller {
         main.changeView(rootPane, fxmlFileName);
     }
 
+    public Button createButton(String name) {
+        // Denne lager og returnerer en Button.
+        final Button button = new Button(name);
+        button.setId("arrScenes");
+        button.setPrefSize(200,20);
+        button.setOnMouseClicked(event -> {
+            System.out.println(name);
+        });
+        return button;
+    }
+
+    private void putItemsInLists() {
+        //Denne legger til alle unike band i listen på første tab.
+        for (String band : getAllBandsObservableList()) {
+            Button btn = createButton(band);
+            vBoxBands.getChildren().add(btn);
+        }
+    }
 }
